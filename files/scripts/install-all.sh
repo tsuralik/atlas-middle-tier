f75737833eb462640755008c8d473deb2a9ee6b6
“Todd <tsuralik@gmail.com>
2015-04-09 13:35:20 -0400

    added scripts to install mysql-server and a shortcut script to run all installer scripts

---
+++ files/scripts/install-all.sh
@@ -0,0 +1,13 @@
+echo "installing everything..."
+
+source ./install-tree.sh
+source ./install-unzip.sh
+source ./install-httpd.sh
+source ./install-java.sh
+source ./install-mysql-server.sh
+source ./install-tomcat.sh
+source ./install-zookeeper.sh
+source ./install-solr.sh
+source ./install-releases.sh
+
+echo "everything installed"
---
+++ files/scripts/install-mysql-server.sh
@@ -0,0 +1,6 @@
+echo "installing mysql-server..."
+
+echo install [mysql-server] via yum
+sudo yum install mysql-server
+
+echo "mysql-server installation complete"
