ae3c169e935723e80e3f0c1ca1a6350929b6d85b
“Todd <tsuralik@gmail.com>
2015-04-13 13:55:45 -0400

    updated to handle GETs and POSTs

--- src/com/datatactics/l3/fcc/atlas/ecfs/servlets/SubmitComment.java
+++ src/com/datatactics/l3/fcc/atlas/ecfs/servlets/SubmitComment.java
@@ -42,88 +42,85 @@ public class SubmitComment extends HttpServlet {
         
         return retVal;
     }
+    
+    protected void processCommentSubmission(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+        System.err.println("requestURL   : " + request.getRequestURL());
+        System.err.println("queryString  : " + request.getQueryString());
+      
+
+        String zookeeperIP = request.getServletContext().getInitParameter("zookeeper.IP");
+        int zookeeperPort  = Integer.parseInt(request.getServletContext().getInitParameter("zookeeper.port"));
+        
+
+        String proceedingNumber   = getParameter(request, "proceedingNumber");
+        String filer              = getParameter(request, "filer");
+        String email              = getParameter(request, "email");
+        String street             = getParameter(request, "street");
+        String street2            = getParameter(request, "street2");
+        String city               = getParameter(request, "city");
+        String state              = getParameter(request, "state");
+        String zip                = getParameter(request, "zip");
+        String zipExt             = getParameter(request, "zipExt");
+        String comments           = getParameter(request, "comments");
+        
+        
+        Writer writer = response.getWriter();
+        try {
+            SolrProxy solr = new SolrProxy(zookeeperIP, zookeeperPort, "ECFS");
+            DocumentFactory documentFactory = new DocumentFactory();
+            documentFactory.setApplicant(filer);
+            documentFactory.setApplicant_Sort(DEFAULT_VALUE);
+            documentFactory.setBrief(TRUE);
+            documentFactory.setCity(city);
+            //documentFactory.setDateRcpt(date);
+            //documentFactory.setDisseminated(date);
+            documentFactory.setExParte(FALSE);
+            documentFactory.setId("2");
+            //documentFactory.setModified(date);
+            documentFactory.setPages("1");
+            documentFactory.setProceeding(proceedingNumber);
+            documentFactory.setRegFlexAnalysis(FALSE);
+            documentFactory.setSmallBusinessImpact(FALSE);
+            documentFactory.setStateCd(state);
+            documentFactory.setSubmissionType("COMMENT");
+            documentFactory.setText(comments);
+            documentFactory.setViewingStatus("Unrestricted");
+            documentFactory.setZip(zip);
+            
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", "3");
+
+            solr.instance().add(documentFactory.createSolrInputDocument());
+            solr.instance().commit();
+            writer.write(jsonObject.toJSONString());
+        } catch(Exception e) {
+            String msg = e.getMessage();
+            writer.write(msg);
+
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-//      System.err.println("request      : " + request);
-      System.err.println("requestURL   : " + request.getRequestURL());
-      System.err.println("queryString  : " + request.getQueryString());
-//      System.err.println("requestURI   : " + request.getRequestURI());
-//      System.err.println("serverName   : " + request.getServerName());
-//      System.err.println("serverPort   : " + request.getServerPort());
-//      System.err.println("servletPath  : " + request.getServletPath());
-//      System.err.println("parameterMap : " + request.getParameterMap());
-      
-
-      String zookeeperIP = request.getServletContext().getInitParameter("zookeeper.IP");
-      int zookeeperPort  = Integer.parseInt(request.getServletContext().getInitParameter("zookeeper.port"));
-      
-
-      String proceedingNumber   = getParameter(request, "proceedingNumber");
-      String filer              = getParameter(request, "filer");
-      String email              = getParameter(request, "email");
-      String street             = getParameter(request, "street");
-      String street2            = getParameter(request, "street2");
-      String city               = getParameter(request, "city");
-      String state              = getParameter(request, "state");
-      String zip                = getParameter(request, "zip");
-      String zipExt             = getParameter(request, "zipExt");
-      String comments           = getParameter(request, "comments");
-      
-      
-      Writer writer = response.getWriter();
-      try {
-          SolrProxy solr = new SolrProxy(zookeeperIP, zookeeperPort, "ECFS");
-          DocumentFactory documentFactory = new DocumentFactory();
-          documentFactory.setApplicant(filer);
-          documentFactory.setApplicant_Sort(DEFAULT_VALUE);
-          documentFactory.setBrief(TRUE);
-          documentFactory.setCity(city);
-          //documentFactory.setDateRcpt(date);
-          //documentFactory.setDisseminated(date);
-          documentFactory.setExParte(FALSE);
-          documentFactory.setId("2");
-          //documentFactory.setModified(date);
-          documentFactory.setPages("1");
-          documentFactory.setProceeding(proceedingNumber);
-          documentFactory.setRegFlexAnalysis(FALSE);
-          documentFactory.setSmallBusinessImpact(FALSE);
-          documentFactory.setStateCd(state);
-          documentFactory.setSubmissionType("COMMENT");
-          documentFactory.setText(comments);
-          documentFactory.setViewingStatus("Unrestricted");
-          documentFactory.setZip(zip);
-          
-          JSONObject jsonObject = new JSONObject();
-          jsonObject.put("id", "3");
-
-          solr.instance().add(documentFactory.createSolrInputDocument());
-          solr.instance().commit();
-          writer.write(jsonObject.toJSONString());
-      } catch(Exception e) {
-          String msg = e.getMessage();
-          writer.write(msg);
-
-          e.printStackTrace();
-      } finally {
-          try {
-              if (writer != null) {
-                  writer.close();
-              }
-          } catch(Exception e) {
-              e.printStackTrace();
-          }
-      }
+        processCommentSubmission(request, response);
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+        processCommentSubmission(request, response);
     }
 
 }
