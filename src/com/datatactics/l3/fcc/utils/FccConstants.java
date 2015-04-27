1a68333057fa2c56e6885fa539f9c9dc7978d9a8
“Todd <tsuralik@gmail.com>
2015-04-27 12:35:57 -0400

    created constants file

---
+++ src/com/datatactics/l3/fcc/utils/FccConstants.java
@@ -0,0 +1,15 @@
+package com.datatactics.l3.fcc.utils;
+
+public interface FccConstants {
+
+    public final String SOLR_HOST          = "solr.ip";
+    public final String SOLR_PORT          = "solr.port";
+    public final String ZOOKEEPER_HOST     = "zookeeper.ip";
+    public final String ZOOKEEPER_PORT     = "zookeeper.port";
+
+    public final String MYSQL_USER          = "mysql.user";
+    public final String MYSQL_PASSWORD      = "mysql.password";
+    public final String MYSQL_SERVER        = "mysql.server";
+    public final String MYSQL_PORT          = "mysql.port";
+    public final String MYSQL_DATABASE      = "mysql.database";
+}
